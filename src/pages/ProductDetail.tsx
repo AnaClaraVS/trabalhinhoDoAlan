3f03cbca2dbf53ddd17437b8c2060c48ab981f58
Ana Sanches <clara.veronez@eaportal.org>
2026-04-15 14:04:35 -0300

    Base da pag do produto

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,4 +1,24 @@
 const ProductDetail =({...props})=>{
-    return<div {...props}>ProductDetail</div>
-}
+    return (<div {...props}>
+    
+    <section className="viewProduct">
+        <div className="imgPrincipal"> //item img//</div>
+        <div className="infoProduct">
+            <h2> Nome item </h2>
+            <h1> R$preço</h1>
+            <h3> 12x R$preço parcelado</h3>
+            <p> Cor: </p>
+            <div className='opcColor'>
+                //img cor variação//
+            </div>
+            <p>Tamanho:</p>
+            <div className='opcTamanho'>
+                //opc variação tamanho//
+            </div>
+            <h2>Frete</h2>
+        </div>
+    </section>
+    
+    </div>
+)}
 export default ProductDetail
