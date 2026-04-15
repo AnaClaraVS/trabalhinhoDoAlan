0495eee7181414623fb81a99fb0953969debc508
PedroRSanti <pedrorafaelsanti2009@gmail.com>
2026-04-15 14:04:43 -0300

    Começo do Checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,26 @@
 const Checkout =({...props})=>{
-    return<div {...props}>Checkout</div>
+    return <><div {...props}>Checkout
+    <h2>Endereço:</h2>
+    <form>
+        <div className="m-3 block">
+            <label htmlFor="cep">CEP:</label>
+            <input type="number" id="cep" name="Nome" placeholder="CEP" min={0} required></input>
+        </div>
+
+        <div className="m-3 block">
+            <label htmlFor="rua">Rua:</label>
+            <input type="text" id="rua" name="Rua" placeholder="Rua" required></input>
+        </div>
+
+       <div className="m-3">
+            <label htmlFor="numero">Número:</label>
+            <input type="number" id="numero" name="numeroCasa" placeholder="Número" min={0} required></input>
+       </div>
+
+       <div className="">
+            <label htmlFor="complemento">Complemento:</label>
+            <input type="text" id="complemento" name="complemento" placeholder="Complemento" required></input>
+       </div>
+    </form></div></>
 }
 export default Checkout
