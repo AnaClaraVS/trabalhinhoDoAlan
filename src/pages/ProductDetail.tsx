b271537b5cca03e284c4a5333b05a1b2fbba72bd
AnaSanches <clara.veronez@eaportal.org>
2026-04-28 16:42:33 -0300

    ProductDetail atualizado

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,7 +2,7 @@ const ProductDetail =({...props})=>{
     return (<div {...props}>
 
 
-   <section className='flex pt-20'>     {/*TUDO (menos avaliação)*/}
+   <section className='flex pt-20 pb-50'>     {/*TUDO (menos avaliação)*/}
         <section  className='flex flex-col'>         {/* Esquerda */}
                 <div className="imgPrincipal">
                      <img className='h-130 pr-50'src='https://img.lojasrenner.com.br/item/927901963/original/12.jpg'></img></div>
@@ -14,10 +14,10 @@ const ProductDetail =({...props})=>{
         </section>
 
         <section className='flex flex-col gijustify-items-start'>          {/* Direita */}
-            <section className="BuyWindow,">    
+            <section className="pb-15">    
                 <div className='items-start'>
                     <p className='text-3xl text-black pb-5'> Nome item </p>
-                    <h1 className='text-black pb-15'> R$preço</h1>
+                    <p className='text-black pb-15 text-4xl font-[600]'> R$preço</p>
                     <p className='text-black'> Cor: </p>
                     <div className='opcColor'>
                          //img cor variação//
@@ -26,16 +26,16 @@ const ProductDetail =({...props})=>{
                     <div className='opcTamanho'>
                             //opc variação tamanho com img//
                     </div>
-                    <h2 className='pt-10'>Frete</h2>
-                    <p className='text-black'> chegará entre .. e ..</p>
+                    <h2 className='pt-10'>Frete Grátis</h2>
+                    <p className='text-black pb-9'> chegará entre .. e ..</p>
                     <button className='text-black'> comprar agora </button><br></br>
                     <button className='text-black'> adicionar ao carrinho</button>
                 </div>
             </section>
             <div className="infoLoja">
                 //icon loja//
-                <p> nome loja</p>
-                <h2> formas de pagamento</h2>
+                <p className='text-black'> nome loja</p>
+                <p className='text-black'> formas de pagamento</p>
                 //icon de bancos/pix //
             </div>
         </section>
