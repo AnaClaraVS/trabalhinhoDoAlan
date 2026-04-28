a2292335dcf3ba79440be6d212506e6fee6625db
AnaSanches <clara.veronez@eaportal.org>
2026-04-28 16:20:13 -0300

    ProductsPage atualizado

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,36 +1,35 @@
 const ProductDetail =({...props})=>{
     return (<div {...props}>
-    
-    <section className="viewProduct">
-        <section className="Esquerda">
-            <section className="ProducInfo">
-                <div className="imgPrincipal"> //item img//</div>
+
+
+   <section className='flex pt-20'>     {/*TUDO (menos avaliação)*/}
+        <section  className='flex flex-col'>         {/* Esquerda */}
+                <div className="imgPrincipal">
+                     <img className='h-130 pr-50'src='https://img.lojasrenner.com.br/item/927901963/original/12.jpg'></img></div>
+            <section className='flex flex-col items-start'>
                 <h2> Características</h2>
-                <h3> Marca:</h3>
-                <h3> Tipo:</h3>
-                <h3> Descrição:</h3>
+                <h3 className='text-black'> Descrição:</h3>
                 <p> //descrição do item//</p>
             </section>
         </section>
 
-        <section className="Direita">
-            <section className="BuyWindow">
-                <div className="infoProduct">
-                    <h2> Nome item </h2>
-                    <h1> R$preço</h1>
-                    <h3> 12x R$preço parcelado</h3>
-                    <p> Cor: </p>
+        <section className='flex flex-col gijustify-items-start'>          {/* Direita */}
+            <section className="BuyWindow,">    
+                <div className='items-start'>
+                    <p className='text-3xl text-black pb-5'> Nome item </p>
+                    <h1 className='text-black pb-15'> R$preço</h1>
+                    <p className='text-black'> Cor: </p>
                     <div className='opcColor'>
                          //img cor variação//
                     </div>
-                    <p>Tamanho:</p>
+                    <p className='text-black'>Tamanho:</p>
                     <div className='opcTamanho'>
                             //opc variação tamanho com img//
                     </div>
-                    <h2>Frete</h2>
-                    <p> chegará entre .. e ..</p>
-                    <button> comprar agora </button>
-                    <button> adicionar ao carrinho</button>
+                    <h2 className='pt-10'>Frete</h2>
+                    <p className='text-black'> chegará entre .. e ..</p>
+                    <button className='text-black'> comprar agora </button><br></br>
+                    <button className='text-black'> adicionar ao carrinho</button>
                 </div>
             </section>
             <div className="infoLoja">
@@ -40,11 +39,11 @@ const ProductDetail =({...props})=>{
                 //icon de bancos/pix //
             </div>
         </section>
+    </section>
         <div className="Avaliações">
             <h2> Avaliações</h2>
             //estrelinhas avaliações *****
         </div>
-    </section>
 
 
     
