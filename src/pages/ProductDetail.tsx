541f8f45d44813899138d35164bc21ee321f9dce
AnaSanches <clara.veronez@eaportal.org>
2026-04-29 17:55:33 -0300

    ProductsDetail formas de pagamento + avaliações

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,7 +2,7 @@ const ProductDetail = ({ ...props }) => {
     return (<div {...props}>
 
 
-        <section className='flex pt-20 pb-50'>     {/*TUDO (menos avaliação)*/}
+        <section className='flex pt-20 pb-25'>     {/*TUDO (menos avaliação)*/}
             <section className='flex flex-col '>         {/* Esquerda */}
                 <div className="imgPrincipal">
                     <img className='h-130 pr-25 pl-25' src='https://img.lojasrenner.com.br/item/927901963/original/12.jpg'></img></div>
@@ -14,11 +14,14 @@ const ProductDetail = ({ ...props }) => {
             </section>
 
             <section className='flex flex-col justify-items-start w-75 text-left'>          {/* Direita */}
-                <section className="pb-15 border-2 border-[#d2c4b4] rounded-lg ">
+                <section className="pb-6 border-2 border-[#d2c4b4] rounded-lg mb-10">
                     <div className='items-start'>
                         <p className='text-3xl  pb-5 font-[500]'> Nome item </p>
                         <p className=' text-4xl font-[700]'> R$preço</p>
-                        <p className='text-blue-500  text-xl pb-15'>★★★★★</p>
+                        <div className='flex items-center gap-5'>
+                            <p className='text-blue-500  text-xl'>★★★★★</p>
+                            <p> 4.9 (1000 avaliações) </p>
+                        </div>
                         <p className='font-[600]'> Cor: </p>
                         <div className='opcColor'>
                          //img cor variação//
@@ -39,17 +42,43 @@ const ProductDetail = ({ ...props }) => {
                         </div>
                     </div>
                 </section>
-                <div className="infoLoja">
-                //icon loja//
-                    <p> nome loja</p>
-                    <p> formas de pagamento</p>
-                //icon de bancos/pix //
+                <div className="infoLoja pb-10 border-2 border-[#d2c4b4] rounded-lg ">
+                    <p className="text-black text-lg font-[600]"> Formas de Pagamento</p>
+                    <div className='flex flex-col items-start gap-2'>
+                        <p> Pix:</p>
+                        <img className="w-12 h-12" src="https://logospng.org/download/pix/logo-pix-icone-1024.png"></img>
+                        <p> Boleto Bancário:</p>
+                        <img className="w-12 h-8" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8eMvTahW6nLHEMUhGSaU17jnNC-1SCfLKRA&s"></img>
+                        <p> Cartões de Crédito:</p>
+                        <img className="w-15 h-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Elo_card_association_logo_-_black_text.svg/3840px-Elo_card_association_logo_-_black_text.svg.png"></img>
+                        <img className="w-15 h-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Visa_Inc._logo_%282021%E2%80%93present%29.svg/3840px-Visa_Inc._logo_%282021%E2%80%93present%29.svg.png"></img>
+                        <img className="w-12 h-8" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Mastercard-logo.png/330px-Mastercard-logo.png"></img>
+                    </div>
+
+
                 </div>
             </section>
         </section>
-        <div className="Avaliações">
             <h2> Avaliações</h2>
-            //estrelinhas avaliações ★★★★★
+        <div className="Avaliações flex items-center justify-start gap-5 pb-10">
+            <div className="avaliacao bg-[#fcf1e3] rounded-lg p-5 border-2 border-[#d2c4b4] w-80 flex">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmRLRMXynnc7D6-xfdpeaoEUeon2FaU0XtPg&s"
+                    className="h-15 rounded-full"></img>
+                    <p className='text-blue-500  text-xl'>★★★★★</p>
+                    <p> Muito bom! </p>
+            </div>
+            <div className="avaliacao bg-[#fcf1e3] rounded-lg p-5 border-2 border-[#d2c4b4] w-80 flex">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmRLRMXynnc7D6-xfdpeaoEUeon2FaU0XtPg&s"
+                    className="h-15 rounded-full"></img>
+                    <p className='text-blue-500  text-xl'>★★★★★</p>
+                    <p> Chegou rápido e o produto é ótimo! </p>
+            </div>
+            <div className="avaliacao bg-[#fcf1e3] rounded-lg p-5 border-2 border-[#d2c4b4] w-80 flex">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmRLRMXynnc7D6-xfdpeaoEUeon2FaU0XtPg&s"
+                    className="h-15 rounded-full"></img>
+                    <p className='text-blue-500  text-xl'>★★★★★</p>
+                    <p> Produto de qualidade, super recomendo! </p>
+            </div>
         </div>
 
 
