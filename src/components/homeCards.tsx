56415fa0ce6d95e96cd2e3d528b7f03ef65af620
lara englerth <lara.englerth@eaportal.org>
2026-06-02 15:58:51 -0300

    estetica cards

--- src/components/homeCards.tsx
+++ src/components/homeCards.tsx
@@ -10,8 +10,8 @@ const HomeCards = ({ item }: HomeCardsProps) => {
     return (
         <div key={item.id} className="w-30 p-2 cursor-pointer flex justify-center items-center flex-col ">
             <img src={item.images[0]} alt={item.title} className="w-32 h-32 object-cover" />
-            <p className="font-[600]">{item.title}</p>
-            <p>R${item.price},00</p>
+            <p className="font-[500] ">{item.title}</p>
+            <p className='bg-[#4c698c] text-black p-2 rounded-lg font-[700]'>R${item.price},00</p>
         </div>
     );
 };
