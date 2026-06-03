6a69d927f2e9182796646b962cd5559d18c56a45
PauloPazette123 <paulinho.pazette@gmail.com>
2026-06-03 14:00:33 -0300

    Add files via upload

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,6 @@
 import useNavigation from "../hooks/useNavigation"
-import { useStore } from "../hooks/useStore" 
+import { useStore } from "../hooks/useStore"
+import CartItem from "../components/cartItens" 
 
 const Cart =({...props}) => {
 const {cart} = useStore()
@@ -10,25 +11,7 @@ const {cart} = useStore()
             <h2>Carrinho</h2>
 
             {cart.length>0 && cart.map((item)=>(
-
-              <div className="border m-7.5 p-3.75 flex">
-            <img className="h-87.5" src={item.images[0]} alt={item.title} />
-            <div className="flex w-full">
-                <div className="flex flex-col items-start p-4 w-full">
-                    <h2 className="mb-2">{item.title}</h2>
-                    <div className="flex flex-col justify-end items-start h-full">
-                        <h4>Tamanho: Já definido</h4>
-                        <h4 className="mb-2">Cor: Já definido</h4>
-                        <input className="w-10 border-2" type="number" id="Quantidade" name="Quantidade" placeholder="Quantidade" min={0} required
-                        />
-                    </div>
-                </div>
-                <div className="flex py-4 items-end">
-                    <p className="text-4xl text-black font-semibold">R${(item.price).toFixed(2)}</p>
-                </div>
-            </div>
-        </div>
-
+                <CartItem key={item.id} product={item} />
             )
         )}
 
